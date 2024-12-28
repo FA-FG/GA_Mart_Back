382372e2c67a27dfeef75e25167333bb772a8540
Fatima <fatoomi.1999@gmail.com>
2024-12-28 22:48:02 +0300

    backend files

---
+++ routes/CartRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const controller = require('../controllers/CartController')
+const middleware = require('../middleware')
+
+
+
+module.exports = router
---
+++ routes/ProductRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const controller = require('../controllers/ProductController')
+const middleware = require('../middleware')
+
+
+module.exports = router
