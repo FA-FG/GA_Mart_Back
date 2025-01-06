2bac376d08cc6e6a01a552451a03a2b37ce9a3e7
Fatima <fatoomi.1999@gmail.com>
2025-01-06 15:03:05 +0300

    admin server

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
 const express = require('express')
+const app = express()
 const logger = require('morgan')
 const cors = require('cors')
-
-
-const AuthRoutes = require('./routes/AuthRouter');
-const productRoutes = require('./routes/ProductRouter');
-const cartRoutes = require('./routes/CartRouter');
-const orderRoutes = require('./routes/OrderRouter');
-
-
-
-const PORT = process.env.PORT || 5000
-
+const initializeAdminUser = require('./initializeAdmin')
 const db = require('./db')
+const authRoutes = require('./routes/authRoutes')
+const productRoutes = require('./routes/productRoutes')
+const cartRoutes = require('./routes/cartRoutes')
+const orderRoutes = require('./routes/orderRoutes')
 
-const app = express()
+const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use('/auth', authRoutes)
+app.use('/products', productRoutes)
+app.use('/cart', cartRoutes)
+app.use('/orders', orderRoutes)
 
-app.use('/auth', AuthRoutes);
-app.use('/orders', orderRoutes);
-app.use('/products', productRoutes); 
-app.use('/cart', cartRoutes);
-
-
-
+initializeAdminUser()
 app.use('/', (req, res) => {
-  res.send(`Connected!`)
+  res.send(`You are connected to the backend.`)
 })
 
 app.listen(PORT, () => {
   console.log(`Running Express server on Port ${PORT} `)
+})
+
+db.on('error', (error) => {
+  console.error('Database connection error:', error)
 })
