fd27f3f60e06224161a4f3b9ce17ebdbe337d4c5
Fatima <fatoomi.1999@gmail.com>
2025-01-06 15:02:12 +0300

    adding data

--- db/index.js
+++ db/index.js
@@ -4,13 +4,11 @@ require('dotenv').config()
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Successfully connected to MongoDB . . .')
+    console.log('Successfully connected to MongoDB....')
   })
   .catch((e) => {
-    console.error('Connection error', e.message)
+    console.log('Connection error', e.message)
   })
-
-// mongoose.set('debug', true)
 
 const db = mongoose.connection
 
